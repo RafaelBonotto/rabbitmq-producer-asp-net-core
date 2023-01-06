9310b2be65ff8d5433a9b97b264e87ff9aff7e60
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-01-06 17:07:00 -0300

    Post Mensagem

--- src/RabbitMQProducer/RabbitMQProducer/Controllers/ProducerController.cs
+++ src/RabbitMQProducer/RabbitMQProducer/Controllers/ProducerController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQProducer.Models;
+using System.Text;
+using System.Text.Json;
 
 namespace RabbitMQProducer.Controllers
 {
@@ -7,5 +10,39 @@ namespace RabbitMQProducer.Controllers
     [ApiController]
     public class ProducerController : ControllerBase
     {
+        [HttpPost]
+        public IActionResult InsertMensagem([FromBody] Mensagem msg)
+        {
+            try
+            {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "minhafila1",
+                                durable: true,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+                    var message = JsonSerializer.Serialize(msg);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "minhafila1",
+                                         basicProperties: properties,
+                                         body: body);
+                }
+
+                return Ok(" Tarefa cadastrada na fila");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
 }
